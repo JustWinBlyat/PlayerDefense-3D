d015f6ad934c22ea601405c524da9587a0e1def5
JustWinBlyat <adam.saleh2610@gmail.com>
2023-09-23 20:35:16 -0400

    SpawnManager script has been created.

--- 3D-Defense/Assets/Scripts/SpawnManager.cs
+++ 3D-Defense/Assets/Scripts/SpawnManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject enemyPrefab;
+    public float spawnInterval = 2.0f;
+    public float spawnRange = 20.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnLoop());
     }
 
     // Update is called once per frame
@@ -15,4 +19,22 @@ public class SpawnManager : MonoBehaviour
     {
         
     }
+
+    IEnumerator SpawnLoop()
+    {
+        while (true)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    public void SpawnEnemy()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 spawnPosition = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+    }    
 }
